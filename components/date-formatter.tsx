75a580864703b3fe7776896139463a5c4073ccba
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-07-01 12:52:20 +0700

    feat: update blog with first post

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -1,9 +1,12 @@
+import { parseISO, format } from 'date-fns'
+
 type Props = {
   dateString: string
 }
 
 const DateFormatter = ({ dateString }: Props) => {
-  return <time dateTime={dateString}>{dateString}</time>
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
 }
 
 export default DateFormatter
