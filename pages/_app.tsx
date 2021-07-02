447fee1b6f74c54e48258fa83e7581cc2564194b
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-07-02 14:19:37 +0700

    feat: add GA analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as gtag from '../utils/gtag'
+
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  /**
+   * We follow guides from this repo
+   * Repo: https://github.com/montezume/nextjs-google-analytics
+   * Repo: https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+   * Medium Article: https://medium.com/frontend-digest/using-nextjs-with-google-analytics-and-typescript-620ba2359dea
+   */
+  useEffect(() => {
+    // Only trigger Google Analytics on production environment
+    if (process.env.NODE_ENV === 'production') {
+      const handleRouteChange = (url: URL) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }
+
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,35 @@
+import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../utils/gtag'
 export default class MyDocument extends Document {
   render() {
+    const { isDevelopment } = this.props
+
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {!isDevelopment && (
+            <React.Fragment>
+              {/* Global site tag (gtag.js) - Google Analytics */}
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', '${GA_TRACKING_ID}');
+                  `
+                }}
+              />
+            </React.Fragment>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
