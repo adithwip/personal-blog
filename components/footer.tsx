72b98e897d595c55cd951a138d0ea178aa52ffa5
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2021-07-03 15:29:45 +0700

    style: update footer informations

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,24 +4,34 @@ const Footer = () => {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+        <div className="py-28 flex flex-col md:flex-row items-center">
+          <h3 className="flex-1 text-4xl md:text-5xl font-bold tracking-tighter leading-tight text-center md:text-center mb-10 md:mb-0 md:pr-4">
+            Let's connect!
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="flex flex-1 justify-center items-center gap-4 md:pl-4 md:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/examples/blog-starter-typescript"
+              href="https://twitter.com/adith_wp"
               target="_blank"
               rel="noopener noreferer"
-              className="mx-3 font-bold hover:underline"
+              className="font-bold hover:underline"
             >
-              View on GitHub
+              Twitter
+            </a>
+            <a
+              href="https://www.linkedin.com/in/adith-widya-pradipta-3a777894/"
+              target="_blank"
+              rel="noopener noreferer"
+              className="font-bold hover:underline"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/adithwip"
+              target="_blank"
+              rel="noopener noreferer"
+              className="font-bold hover:underline"
+            >
+              Github
             </a>
           </div>
         </div>
