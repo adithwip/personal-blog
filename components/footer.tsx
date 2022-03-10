c6a5c02c74b03b431bf3245e8adc2ae1eb9a0eb6
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2022-03-10 08:22:12 +0700

    fix: wrong url rel, avatar with Image component, remark lib

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,37 +6,33 @@ const Footer = () => {
       <Container>
         <div className="flex flex-col items-center py-28 md:flex-row">
           <h3 className="mb-10 flex-1 text-center text-4xl font-bold leading-tight tracking-tighter text-indigo-600 md:mb-0 md:pr-4 md:text-center md:text-5xl">
-            Let's connect!
+            {"Let's connect!"}
           </h3>
           <div className="flex flex-1 items-center justify-center gap-4 md:w-1/2 md:pl-4">
             <a
               href="https://twitter.com/adith_wp"
-              target="_blank"
-              rel="noopener noreferer"
+              rel="noreferrer"
               className="font-bold text-indigo-600 hover:underline"
             >
               Twitter
             </a>
             <a
               href="https://www.linkedin.com/in/adith-widya-pradipta-3a777894/"
-              target="_blank"
-              rel="noopener noreferer"
+              rel="noreferrer"
               className="font-bold text-indigo-600 hover:underline"
             >
               Linkedin
             </a>
             <a
               href="https://www.polywork.com/adith"
-              target="_blank"
-              rel="noopener noreferer"
+              rel="noreferrer"
               className="font-bold text-indigo-600 hover:underline"
             >
               Polywork
             </a>
             <a
               href="https://github.com/adithwip"
-              target="_blank"
-              rel="noopener noreferer"
+              rel="noreferrer"
               className="font-bold text-indigo-600 hover:underline"
             >
               Github
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,8 +1,8 @@
 const Intro = () => {
   return (
     <section className="mt-16 mb-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
-      <h1 className="text-6xl font-bold leading-tight tracking-tighter text-indigo-600 text-indigo-600 md:pr-8 md:text-8xl">
-        Adith's Blog.
+      <h1 className="text-6xl font-bold leading-tight tracking-tighter text-indigo-600 md:pr-8 md:text-8xl">
+        {"Adith's Blog."}
       </h1>
       <h4 className="mt-5 text-center text-lg md:pl-8 md:text-left">
         A statically generated blog example using{' '}
